46e1919250394eb7ae66db8ed73d37cc72f7678f
eva.wang <eva.wang81@gmail.com>
2024-10-11 11:14:47 +0800

    deployed

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,11 @@ require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.27",
+  networks: {
+    sepolia: {
+      chainId: 11155111,
+      url: process.env.ALCHEMY_TESTNET_RPC_URL,
+      accounts: [process.env.TESTNET_PRIVATE_KEY],
+    },
+  },
 };
